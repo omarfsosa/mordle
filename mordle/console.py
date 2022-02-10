eb051ed43ce81e05f959632565026c3a8592a68f
Omar Sosa <omarfsosa@gmail.com>
2022-02-10 19:05:33 +0000

    console update

--- mordle/console.py
+++ mordle/console.py
@@ -2,7 +2,8 @@ import click
 import random
 
 from mordle.corpus import Corpus
-from mordle.game import Game
+from mordle.game import Game, Result
+from mordle.patterns import Pattern
 from mordle import player
 
 
@@ -20,12 +21,12 @@ def wordle():
     """
 
 
-@wordle.command("play")
+@wordle.command("local")
 @click.option("--auto", "plyr", flag_value="Bot", help="Let a bot play the game")
 @click.option("--human", "plyr", flag_value="Human", help="Let a human play the game")
 @click.option("--semi", "plyr", flag_value="SemiHuman", help="Let a semi-human play the game")
 @click.option("--answer", type=str, default="", help="Force the answer")
-def new_game(plyr, answer):
+def local(plyr, answer):
     if not answer:
         answer = random.choice(Corpus())
     else:
@@ -33,3 +34,18 @@ def new_game(plyr, answer):
 
     game = Game(getattr(player, plyr)(), answer)
     game.run()
+
+
+@wordle.command("online")
+def online():
+    bot = player.Bot()
+    while True:
+        guess = click.prompt("guess: ").lower()
+        values = [int(x) for x in click.prompt("pattern: ").split()]
+        print(values)
+        result = Result(guess, Pattern(*values))
+        if result.is_correct():
+            break
+
+        bot.update(result)
+        click.echo(f"Best guess is {bot.best_guess}")
